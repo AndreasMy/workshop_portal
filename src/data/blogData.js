61f70d6d1cfaa8b36ee502b4405e3461c01d6fd8
AndreasMy <amykle85@gmail.com>
2025-03-16 20:43:19 +0100

    Removed old workshops ad and added a new one

--- src/data/blogData.js
+++ src/data/blogData.js
@@ -1,22 +1,23 @@
 export const entryData = [
-  /* {
-    title: "Ansiktets Anatomi",
-    href: "https://www.kursagenten.no/enkeltkurs/240831-Tegnekurs-Ansiktets-anatomi-med-Andreas-Myk-209811",
+  {
+    title: "Figurativ Tegning fra Foto - Rytmer og Struktur",
+    href: "https://www.kursagenten.no/enkeltkurs/250426-Figurativ-tegning-fra-foto-rytmer-struktur--146963",
     buttonText: "Til Påmelding",
     slideContent: [
       {
         id: 1,
         media: {
-          src: "/workshop_portal/images/drawings/skulls.jpg",
+          src: "/workshop_portal/videos/blockin-rythms-web.mp4",
           alt: "descriptive text",
           className: "slide-video",
         },
         text: {
           header: "Om kurset:",
           paragraphs: [
-            "Dette lynkurset gir deg en innføring i ansiktets anatomiske oppbygning.",
-            "Vi vil utforske viktige anatomiske landemerker som defineres av skjelettet og muskulaturen.",
-            "Vi vil også lære å benytte oss av konstruksjonstegning for å forenkle denne informasjonen visuelt.",
+            "Dette er et nybegynner kurs i figurativ tegning.",
+            "Vi lærer litt om strek, lys og skygge, form og proposjoner",
+            "Vi vil utforske viktige anatomiske landemerker som defineres av skjelettet og muskulaturen",
+            "Vi vil også lære å forenkle denne informasjonen visuelt",
           ],
         },
       },
@@ -67,98 +68,7 @@ export const entryData = [
       },
       {
         name: "Dato: ",
-        value: "31/08 - 01/09",
-      },
-      {
-        name: "Tid: ",
-        value: "10:00 - 16:00",
-      },
-    ],
-    text: {
-      headerText: `Portrett anatomi - ansiktets anatomi og strukturell oppbygning. Kurset er åpent for kunstnere på alle nivåer, fra nybegynnere til erfarne. Det viktigste er at du har lidenskap for tegning og maling, og en nysgjerrighet for å utforske nye muligheter.
-      `,
-      paragraphs: [
-        {
-          header: "Om Kurset:",
-          para: `Dette er et lynkurs som gir deg en grunnleggende innføring i ansiktets anatomiske oppbygning. Vi vil utforske viktige anatomiske landemerker som defineres av skjelettet og muskulaturen. Vi vil også lære å benytte oss av konstruksjonstegning for å forenkle denne informasjonen visuelt. Resultatet av denne type studie er en dypere forståelse av ansiktets form og oppbygning, samt en styrket evne til å tegne/male ansikter fra observasjon eller fantasi.
-      `,
-        },
-      ],
-    },
-  }, */
-  {
-    title: "Portrettkurs Med Modell",
-    href: "https://www.kursagenten.no/enkeltkurs/241102-Malekurs-Portrett-m-Formbeskrivelse-Andreas-204443",
-    buttonText: "Til Påmelding",
-    slideContent: [
-      {
-        id: 1,
-        media: {
-          src: "/workshop_portal/thumbnails/paintings/studio/jordan-1-thumbnail.jpg",
-          alt: "descriptive text",
-          className: "slide-video",
-        },
-        text: {
-          header: "Om kurset:",
-          paragraphs: [
-            "Vi tegner eller maler et protrett etter modell",
-            "Lær å vektlegge det essensielle",
-            "Bli kjent med å jobbe med sight-size metoden",
-            "Prøv deg på å jobbe med block-in metoden",
-          ],
-        },
-      },
-      {
-        id: 2,
-        media: {
-          src: "/workshop_portal/images/photos/materials-small-2.jpg",
-          alt: "descriptive text",
-          className: "slide-video",
-        },
-        text: {
-          header: "Materialer du må ha med deg:",
-          paragraphs: [
-            "Lerret (størrelses forhold 2 : 3 - f.eks 40 x 60 cm)",
-            "Pensler -  Fasong og cs størrelse som i bildet til venstre",
-            "Palett kniv",
-            "Pallett",
-            "Oliven olje og terpentin, om du maler med olje",
-            "Anbefalte farger: Raw Umber, Titanium White, Ivory Black, English Red, Yellow Ochre, Ultramarine Blue",
-          ],
-        },
-      },
-    ],
-    defaultImage: [
-      {
-        image: "/workshop_portal/images/drawings/draw4.jpg",
-        alt: "descriptive text",
-        className: "workshop-image",
-      },
-    ],
-    images: [
-      {
-        image: "/workshop_portal/images/drawings/draw2.jpg",
-        alt: "descriptive text",
-        className: "workshop-image",
-      },
-      {
-        image: "/workshop_portal/images/drawings/draw4.jpg",
-        alt: "descriptive text",
-        className: "workshop-image",
-      },
-    ],
-    info: [
-      {
-        name: "Sted: ",
-        value: "Oslo Malerklubb",
-      },
-      {
-        name: "Pris: ",
-        value: "2500,-",
-      },
-      {
-        name: "Dato: ",
-        value: "02/11 - 03/11",
+        value: "26/04 - 27/04",
       },
       {
         name: "Tid: ",
@@ -177,97 +87,8 @@ export const entryData = [
       ],
     },
   },
-  {
-    title: "Landskapsmaleri Fra Foto ",
-    href: "https://www.kursagenten.no/enkeltkurs/241123-Oljeakryl-malekurs-landskap-etter-foto-Andr-196597",
-    buttonText: "Til Påmelding",
-    slideContent: [
-      {
-        id: 1,
-        media: {
-          src: "/workshop_portal/thumbnails/paintings/landscape/florence-paintings-1-thumbnail.jpg",
-          alt: "descriptive text",
-          className: "slide-video",
-        },
-        text: {
-          header: "Om kurset:",
-          paragraphs: [
-            "Lær deg å formidle det essensielle.",
-            "Lek med å formidle stemninger.",
-            "Bli kjent med grunnleggende bilde komposisjon.",
-            "Utforsk sammenhengen mellom abstraksjon og representasjon i bildet ditt.",
-          ],
-        },
-      },
-      {
-        id: 2,
-        media: {
-          src: "/workshop_portal/images/photos/materials-small-2.jpg",
-          alt: "descriptive text",
-          className: "slide-video",
-        },
-        text: {
-          header: "Materialer du må ha med deg:",
-          paragraphs: [
-            "Lerret (størrelses forhold 2 : 3 - f.eks 40 x 60 cm).",
-            "Pensler -  Fasong og cs størrelse som i bildet til venstre.",
-            "Palett kniv",
-            "Pallett",
-            "Oliven olje og terpentin, om du maler med olje.",
-            "Anbefalte farger: Raw Umber, Titanium White, Ivory Black, English Red, Yellow Ochre, Ultramarine Blue.",
-          ],
-        },
-      },
-    ],
-    defaultImage: [
-      {
-        image: "/workshop_portal/images/drawings/draw4.jpg",
-        alt: "descriptive text",
-        className: "workshop-image",
-      },
-    ],
-    images: [
-      {
-        image: "/workshop_portal/images/drawings/draw2.jpg",
-        alt: "descriptive text",
-        className: "workshop-image",
-      },
-      {
-        image: "/workshop_portal/images/drawings/draw4.jpg",
-        alt: "descriptive text",
-        className: "workshop-image",
-      },
-    ],
-    info: [
-      {
-        name: "Sted: ",
-        value: "Oslo Malerklubb",
-      },
-      {
-        name: "Pris: ",
-        value: "2500,-",
-      },
-      {
-        name: "Dato: ",
-        value: "23/11 - 24/11",
-      },
-      {
-        name: "Tid: ",
-        value: "10:00 - 16:00",
-      },
-    ],
-    text: {
-      headerText: `Portrett anatomi - ansiktets anatomi og strukturell oppbygning. Kurset er åpent for kunstnere på alle nivåer, fra nybegynnere til erfarne. Det viktigste er at du har lidenskap for tegning og maling, og en nysgjerrighet for å utforske nye muligheter.
-      `,
-      paragraphs: [
-        {
-          header: "Om Kurset:",
-          para: `Dette er et lynkurs som gir deg en grunnleggende innføring i ansiktets anatomiske oppbygning. Vi vil utforske viktige anatomiske landemerker som defineres av skjelettet og muskulaturen. Vi vil også lære å benytte oss av konstruksjonstegning for å forenkle denne informasjonen visuelt. Resultatet av denne type studie er en dypere forståelse av ansiktets form og oppbygning, samt en styrket evne til å tegne/male ansikter fra observasjon eller fantasi.
-      `,
-        },
-      ],
-    },
-  },
+
+
   {
     title: "Vikar Fabrikken Tegne og Maleskole",
     href: "https://www.fabrikkentegneogmaleskole.no/",
